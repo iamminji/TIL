a1112867cdc0de8e12a24deed90e3ef2c8d94e5f
iamminji <minji.dev@gmail.com>
2019-01-03 10:42:34 +0900

    리액트 공부

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from 'react';
+import IterationSample from './IterationSample'
+
+class App extends Component {
+    render() {
+        return (
+            <IterationSample/>
+        )
+    }
+}
+
+export default App;
