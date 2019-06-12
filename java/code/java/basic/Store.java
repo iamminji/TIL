079d50544c804412e9b657a6c2c35f9593d46345
iamminji <minji.dev@gmail.com>
2019-06-12 18:36:07 +0900

    20190612

---
+++ java/code/java/basic/Store.java
@@ -0,0 +1,10 @@
+package java.basic;
+
+class Store {
+    private int a;
+
+    public Store() {
+        int b = 1;
+        this.a = 2;
+    }
+}
---
+++ java/junit.md
@@ -0,0 +1,5 @@
+# Junit
+
+
+- `setUp()` 매 method 시작전에 실행된다.
+- `tearDown()` 매 method 끝난 후에 실행된다. 
